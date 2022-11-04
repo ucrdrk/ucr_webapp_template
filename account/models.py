36f447a64e834329d76765dad18cb07335f4dc2a
Albert Nguyen <heiygu@gmail.com>
2022-11-03 20:01:30 -0700

    add account source file

---
+++ account/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to ='avatars/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
