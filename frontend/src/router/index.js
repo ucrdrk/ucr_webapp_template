7588d65dd6c3b4db5475b31f29bd23b6e9e14475
jbayl006 <jbayl006@ucr.edu>
2022-11-30 20:14:13 -0800

    Worked on registration

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 Vue.use(VueRouter);
 
 const routes = [
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,70 +1,16 @@
+
 <template>
-    <div class="register">
-      <NavBar navbartype='register' />
-      <div class="registerbox">
-        <form @submit.prevent="submitForm">
-          <h3 class="registertitle">Register</h3>
-          <p class="prompt">Username</p>
-          <input type="text" class="userinput" v-model="username">
-          <p class="prompt">Email</p>
-          <input type="text" class="userinput" v-model="email">
-          <p class="prompt">Password</p>
-          <input type="password" class="userinput" v-model="password">
-          <p class="prompt">Confirm Password</p>
-          <input type="password" class="userinput" v-model="password2">
-          <button>Submit</button>
-        </form>
-      </div>
-    </div>
-  </template>
+  <div class = "register">
+    <h1>Register</h1>
+    <form @submit.prevent="submitForm">
+      <input type="email" name = "username" v-model = "username">
+      <input type = "password" name = "password" v-model = "password">
+      <button type = "submit">Sign up</button>
+    </form>
+  </div>
+</template>
   
-  <style>
-  .register {
-    height: 100vh;
-    background-color: #1A1D1A;
-  }
-  .registerbox {
-    height: 670px;
-    width: 500px;
-    margin: auto;
-    margin-top: 4em;
-    background-color: white;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-  }
-  .registertitle {
-    text-align: center;
-    margin: 51px auto 55px;
-    font-family: 'Inter', sans-serif;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 40px;
-  }
-  .prompt {
-    margin-left: 15%;
-    margin-bottom: 22px;
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-size: 20px;
-  }
-  .userinput {
-    margin-left: 15%;
-    margin-bottom: 22px;
-    width: 330px;
-    height: 45px;
-    border-radius: 10px;
-    font-size: large;
-  }
-  #submit {
-    width: 330px;
-    height: 55px;
-    margin-left: 15%;
-    margin-top: 30px;
-    border-radius: 30px;
-  }
-  </style>
+
   
   <script>
   import NavBar from '../components/NavBar.vue';
@@ -79,22 +25,25 @@
     data () {
       return {
         username: "",
-        email: "",
         password: "",
-        password2: "",
       }
     },
     methods: {
-        submitForm(){
+        submitForm(e){
           const formData = {
             username: this.username,
             email: this.email,
             password: this.password,
-            password2: this.password2
           }
           axios
               .post('/api/account/register', formData)
-              
+              .then(response => {
+                this.$router.push('/login')
+                console.log(response)
+              })
+              .catch(error=> {
+                console.log(error)
+              })
         }
     }
   };
