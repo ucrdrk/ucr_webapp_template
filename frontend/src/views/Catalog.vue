6c60ddfe750ebd6d469c96348524a56bfdc83869
cooled909 <eddiesegura596@gmail.com>
2023-01-28 21:08:06 -0800

    created catalog

---
+++ frontend/src/views/Catalog.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="site">
+        <NavBar navbartype="login"/>
+        <div class="content">
+            <div class="mystuff">
+                <div class="fpga">
+                    <h2>FPGA board connected</h2>
+                </div>
+                <div class="mygames">
+
+                </div>
+            </div>
+            <div class="catalog">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.site {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    background-color: #1A1D1A;
+}
+.content {
+    height: 100vh;
+    background-color: #ffffff;
+}
+.mystuff {
+
+}
+</style>
+
+<script>
+    import NavBar from "../components/NavBar.vue";
+
+    export default {
+        name: "HomePage",
+        components: {
+            NavBar,
+    },
+  data () {
+    return {
+      values: []
+    }
+  },
+  created: function () {
+    this.$http.get('/api/foos/').then((response) => {
+      console.log(response.data)
+      this.values = response.data
+    })
+    console.log("Created")
+  }
+};
+</script>
