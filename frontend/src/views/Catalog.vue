56e75cc3ed2502d5b2640c307996cf76aef42470
cooled909 <eddiesegura596@gmail.com>
2023-01-28 21:54:22 -0800

    work on catalog frontend

--- frontend/src/views/Catalog.vue
+++ frontend/src/views/Catalog.vue
@@ -4,10 +4,11 @@
         <div class="content">
             <div class="mystuff">
                 <div class="fpga">
-                    <h2>FPGA board connected</h2>
+                    <h2 class="fpgatitle">FPGA board connected</h2>
+                    <div class="sendbutton">Send games to board</div>
                 </div>
                 <div class="mygames">
-
+                    <h2>My Games</h2>
                 </div>
             </div>
             <div class="catalog">
@@ -25,11 +26,40 @@
     background-color: #1A1D1A;
 }
 .content {
+    display: flex;
+    flex-direction: column;
     height: 100vh;
     background-color: #ffffff;
+
 }
 .mystuff {
-
+    display: flex;
+    flex-direction: row;
+    height: 400px;
+    /*background-color: red;*/
+    margin: 30px;
+}
+.fpga {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+}
+.fpgatitle {
+    width: 40%;
+}
+.sendbutton {
+    text-align: center;
+    vertical-align: middle;
+    height: 90px;
+    line-height: 90px;
+    width: 288px;
+    background-color: #6D8B9C;
+    border-radius: 15px;
+    font-size: 24px;
+    margin-top: 20%;
+}
+.mygames {
+    display: flex;
 }
 </style>
 
