b4bcb104a2e82c30582933621c67d2545ca01ab1
JoseC678 <cervantesjose657@gmail.com>
2023-02-26 03:39:54 -0800

    urls dont seem to be working

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls import url
+from account.api import views
 from account.api.views import(
     registration_view,
 )
@@ -9,5 +11,7 @@ app_name = "account"
 
 urlpatterns = [
     path('register', registration_view, name = 'register'),
-    path('login', obtain_auth_token, name = 'login')
+    path('login', obtain_auth_token, name = 'login'),
+    ##url(r'^userGame$',views.userGameAPI),
+    ##url(r'^userGame/([0-9]+)$',views.userGameAPI)
 ]
