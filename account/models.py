195b156b107a7cc45cdd8757721a64e91fde00e4
JoseC678 <cervantesjose657@gmail.com>
2023-02-27 11:31:17 -0800

    added unique user id

--- account/models.py
+++ account/models.py
@@ -6,9 +6,11 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+import uuid
 
 # Create your models here.
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     birth_date = models.DateField(null=True, blank=True)
     avatar = models.ImageField(upload_to ='avatars/', null=True, blank=True)
     game = models.ManyToManyField('games.Games')
