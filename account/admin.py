d4e67f86abe75cff5bbdbb2a6fbab73a2840fb30
JoseC678 <cervantesjose657@gmail.com>
2023-02-27 11:32:30 -0800

    Display user id field

--- account/admin.py
+++ account/admin.py
@@ -9,7 +9,7 @@ from django.contrib.auth.admin import UserAdmin
 class CustomUserAdminPage(UserAdmin):
     model = User
     add_from = CustomUserForm
-    list_display = ('username','email','birth_date','first_name','last_name','is_staff','date_joined')
+    list_display = ('username','email','birth_date','last_name','is_staff','date_joined','id')
 
 
     fieldsets = (
