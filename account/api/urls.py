b64c1a0c4f7ec004cdc5fb7a3657c76e666c9324
JoseC678 <cervantesjose657@gmail.com>
2023-02-26 21:24:31 -0800

    url for userGamesAPI view

--- account/api/urls.py
+++ account/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import url
 from account.api import views
 from account.api.views import(
-    registration_view,
+    registration_view,userGameAPI
 )
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -14,4 +14,5 @@ urlpatterns = [
     path('login', obtain_auth_token, name = 'login'),
     url(r'^userGame$',views.userGameAPI),
     url(r'^userGame/([0-9]+)$',views.userGameAPI)
+    #url(r'^userGame/$', userGameAPI, name='userGame')
 ]
