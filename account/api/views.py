d4c5a22fe8d7de08083c375609bd018956905683
JoseC678 <cervantesjose657@gmail.com>
2023-02-27 18:21:36 -0800

    Able to retrieve specific information from a specified user through userGameAPI

--- account/api/views.py
+++ account/api/views.py
@@ -10,9 +10,12 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from django.http import FileResponse
+
 
 from account.models import User
 from account.api.serializers import UserGameSerializer
+from django.core.files.storage import default_storage
 
 
 
@@ -36,19 +39,33 @@ def registration_view(request):
         return Response(data)
 
 
-#'''
+
 @csrf_exempt
 def userGameAPI(request,id=0):
     if request.method=='GET':
-        user_id = request.GET['username']
-        users = User.objects.all() 
+        #If the payload from the script does no match then it blows up the code. fix later on
+        user_id = request.GET['value']
+        
+        #Checks to see if the value is an actual user
+        #need to make sure its length is 26 characters. if its shorter or longer the function blows up
+        users = User.objects.all().filter(account_id=user_id)
         user_serializer = UserGameSerializer(users,many=True)
         data = user_serializer.data
+        
 
-        #data[0]['username'] # this returns the first username in the whole database.
+        #using FileResponse
+        #file = open(data[0]['game'][1]['rma_file'],'rb')
+        #response = FileResponse(file)
 
-        return JsonResponse(data[0], safe=False)
-#'''
+        ''' 
+        --data-- returns the user of the specific user_id
+        --data[0]['game']-- returns the list of games to that user
+        --data[0]['game'][1]['rma_file']-- returns the file path to the rma file
+        #'''
+
+        return JsonResponse(data[0]['game'][1]['rma_file'], safe=False)
+        #return JsonResponse(data[0]['game'][1]['rma_file'], safe=False)
+
 
 '''
 @csrf_exempt
