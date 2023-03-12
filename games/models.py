02e231ad11be4adf76528332e1fd84a9a22168b9
JoseC678 <cervantesjose657@gmail.com>
2023-03-11 23:31:12 -0800

    added file size field

--- games/models.py
+++ games/models.py
@@ -6,7 +6,7 @@ class Games(models.Model):
     file_location = models.CharField(max_length=200)
     game_year = models.IntegerField()
     cover = models.CharField(max_length=200)
-    game_size = models.FloatField()
+    file_size = models.FloatField()
     rma_file = models.FileField(upload_to='rmaFolder/', blank=True)
     def __str__(self):
         return self.game_name
