682abb66b1cd48b2744e39e661db3a60f47990ea
JoseC678 <cervantesjose657@gmail.com>
2023-03-13 18:42:00 -0700

    Added rbf field to download the file

--- MISTER_Template/Scripts/script.py
+++ MISTER_Template/Scripts/script.py
@@ -86,7 +86,7 @@ class Hashmap:
         f5.close()
     
 def write_Game(text):
-    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', 'https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
+    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', '/media/rbfFolder/jtcps1_20220819.rbf','https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
     game = text.split(',')
 
     #get the rma file replace the %20 characters that show up from .get()
@@ -94,10 +94,27 @@ def write_Game(text):
     split = (game[1].replace('%20',' ')).split('/')
     mra = split[3]
 
+    split = (game[2].replace('%20',' ')).split('/')
+    rbf = split[3]
+
+
+
     #write the mra file
     r4 = requests.get('http://127.0.0.1:8000/media/rmaFolder/' + mra)
     with open('../_Arcade/'+mra,'wb') as fx:
         fx.write(r4.content)
+    python_sucks = 0
+
+    #write the rbf file
+    if os.path.exists('../_Arcade/cores/'+rbf):
+        python_sucks = python_sucks + 1
+    else:
+        print("Writing rbf file")
+        r5 = requests.get('http://127.0.0.1:8000/media/rbfFolder/' + rbf)
+        with open('../_Arcade/cores/'+rbf,'wb') as fy:
+            fy.write(r5.content)
+
+    
 
     #check how many files need to be downloaded subtract off the mra file and game name
     #divide for how many pairs of files and paths
@@ -107,10 +124,10 @@ def write_Game(text):
 
 
     for i in range(num_files):
-        file_link.append(game[i+2]) #plus 2 start at the first download link skip 'game_name' and 'rma'
+        file_link.append(game[i+3]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
     
     for i in range(num_files):
-        file_path.append(game[(num_files+i+2)]) #plus 2 start at the first download link skip 'game_name' and 'rma'
+        file_path.append(game[(num_files+i+3)]) #plus 2 start at the first download link skip 'game_name' and 'rma' and rbf
                                           #plus num_files to skip over the file_link
 
     
@@ -140,12 +157,17 @@ def delete_Game(text):
     split = (game[1].replace('%20',' ')).split('/')
     mra = split[3]
 
+    split = (game[2].replace('%20',' ')).split('/')
+    rbf = split[3]
+
+
     
     #delete MRA file
     if os.path.exists('../_Arcade/'+mra):
         os.remove('../_Arcade/'+mra)
     # else:
-    #     print("File does not exist")
+        #do not remove the rbf file since one core supports multiple arcade games
+        #print("File does not exist")
 
 
     #check how many files need to be downloaded subtract off the mra file and game name
@@ -156,10 +178,10 @@ def delete_Game(text):
     file_path = [] #['games/mame/athena.zip\n']
 
     for i in range(num_files):
-        file_link.append(game[i+2]) #plus 2 start at the first download link skip 'game_name' and 'rma'
+        file_link.append(game[i+3]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
     
     for i in range(num_files):
-        file_path.append(game[(num_files+i+2)]) #plus 2 start at the first download link skip 'game_name' and 'rma'
+        file_path.append(game[(num_files+i+3)]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
                                           #plus num_files to skip over the file_link
 
     # zip_file = file_link[0].split('/') #['https:', '', 'archive.org', 'download', 'MAME216RomsOnlyMerged', '1942.zip']
@@ -178,9 +200,12 @@ def delete_Game(text):
     #delete MRA file
 
 
+#SENDING 2 GAMES [{} ,{}]
+# [{'id': 1, 'game_name': '4D Warriors (315-5162)', 'game_file_link': 'https://archive.org/download/MAME216RomsOnlyMerged/4dwarrio.zip', 'file_location': 'games/mame/4dwarrio.zip', 'game_year': 9999, 'cover': 'https://www.igdb.com/ find the image', 'file_size': 10.0, 'rma_file': '/media/rmaFolder/4D%20Warriors%20(315-5162).mra', 'rbf_file': '/media/rbfFolder/SEGASYS1_20210618.rbf'},
+# {'id': 4, 'game_name': 'Adventure Quiz Capcom World 2 -Japan 920611-', 'game_file_link': 'https://archive.org/download/MAME217RomsOnlyMerged/MAME%200.217%20ROMs%20%28merged%29.zip/cworld2j.zip', 'file_location': 'games/mame/cworld2j.zip', 'game_year': 1941, 'cover': 'https://www.igdb.com/ find the image', 'file_size': 10.0, 'rma_file': '/media/rmaFolder/Adventure%20Quiz%20Capcom%20World%202%20-Japan%20920611-.mra', 'rbf_file': '/media/rbfFolder/jtcps1_20220819.rbf'}]
 
 
-payload = {'value':'03822b5f-3f93-4ad3-be7f-17d430712e45'}
+payload = {'value':'0bc860fd-5d17-4b7a-aa6f-d6315975aab0'}
 
 
 
@@ -216,7 +241,7 @@ try:
 #-------output the web server list to a file and add it to the hash table h_web. 
         #I get division by zero in modulus operator
         for x in range(web_num_games):
-            output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
+            output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
             f.write(output)
             h_web.add(server_list[x]['game_name'], output)
         f.close()
@@ -316,7 +341,7 @@ try:
             f3 = open("misterList.txt","w")
             for x in range(web_num_games):
                 #print("file does not exists!!")
-                output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
+                output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
                 f3.write(output)
                 h_web.add(server_list[x]['game_name'], output) #add to hashmap
                 write_Game(output)
