d0d9175f3087677199c32210afe33df53ec7cd9d
JoseC678 <cervantesjose657@gmail.com>
2023-03-13 17:10:54 -0700

    change size from 200 to 1000

--- games/models.py
+++ games/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Games(models.Model):
-    game_name = models.CharField(max_length=200)
-    game_file_link = models.CharField(max_length=200)
-    file_location = models.CharField(max_length=200)
+    game_name = models.CharField(max_length=1000)
+    game_file_link = models.CharField(max_length=1000)
+    file_location = models.CharField(max_length=1000)
     game_year = models.IntegerField()
-    cover = models.CharField(max_length=200)
+    cover = models.CharField(max_length=1000)
     file_size = models.FloatField()
     rma_file = models.FileField(upload_to='rmaFolder/', blank=True)
     rbf_file = models.FileField(upload_to='rbfFolder/', blank=True)
