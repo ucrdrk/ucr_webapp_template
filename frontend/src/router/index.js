8c36b3f00a2dde6f4f5e5921eec770b5a631fd00
jbayl006 <jbayl006@ucr.edu>
2023-03-14 11:20:38 -0700

    making FAQ page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -37,6 +37,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/mainPage.vue"),
   },
+  {
+    path: "/faqpage",
+    name: "FAQPage",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/FAQPage.vue"),
+  },
 ];
 
 const router = new VueRouter({
