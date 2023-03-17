2068ddd50fcc0811faec13fba9c13b93c66208d6
JoseC678 <cervantesjose657@gmail.com>
2023-03-16 19:35:50 -0700

    Added the fileds added by julia in seed.json

--- games/models.py
+++ games/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
+
+
+
+
+
 class Games(models.Model):
     game_name = models.CharField(max_length=1000)
     game_file_link = models.CharField(max_length=1000)
@@ -9,5 +15,11 @@ class Games(models.Model):
     file_size = models.FloatField()
     rma_file = models.FileField(upload_to='rmaFolder/', blank=True)
     rbf_file = models.FileField(upload_to='rbfFolder/', blank=True)
+    game_type = models.CharField(max_length =1000)
+    manufacturer = models.CharField(max_length=1000)
+    category = models.CharField(max_length = 1000)
+
+
+
     def __str__(self):
         return self.game_name
