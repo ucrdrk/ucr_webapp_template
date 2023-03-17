4258359ea78096d5276fd6fe80a20f811be9181b
JoseC678 <cervantesjose657@gmail.com>
2023-03-17 15:43:08 -0700

    Fixed the error I was getting when the user synced with an empty game list

--- MISTER_Template/Scripts/script.py
+++ MISTER_Template/Scripts/script.py
@@ -5,6 +5,10 @@ import math
 import os
 
 class Hashmap:
+    global ip_addr 
+    ip_addr = '127.0.0.1'
+    global payload 
+    payload = {'value':'dcd167a6-6900-47e5-bf6c-ddb435067995'}
     def __init__(self, arrSize):
         self.size = arrSize
         self.map = [None] * self.size
@@ -100,7 +104,7 @@ def write_Game(text):
 
 
     #write the mra file
-    r4 = requests.get('http://127.0.0.1:8000/media/rmaFolder/' + mra)
+    r4 = requests.get('http://' + ip_addr + ':8000/media/rmaFolder/' + mra)
     with open('../_Arcade/'+mra,'wb') as fx:
         fx.write(r4.content)
     python_sucks = 0
@@ -110,7 +114,7 @@ def write_Game(text):
         python_sucks = python_sucks + 1
     else:
         print("Writing rbf file")
-        r5 = requests.get('http://127.0.0.1:8000/media/rbfFolder/' + rbf)
+        r5 = requests.get('http://'+ ip_addr + ':8000/media/rbfFolder/' + rbf)
         with open('../_Arcade/cores/'+rbf,'wb') as fy:
             fy.write(r5.content)
 
@@ -205,7 +209,7 @@ def delete_Game(text):
 # {'id': 4, 'game_name': 'Adventure Quiz Capcom World 2 -Japan 920611-', 'game_file_link': 'https://archive.org/download/MAME217RomsOnlyMerged/MAME%200.217%20ROMs%20%28merged%29.zip/cworld2j.zip', 'file_location': 'games/mame/cworld2j.zip', 'game_year': 1941, 'cover': 'https://www.igdb.com/ find the image', 'file_size': 10.0, 'rma_file': '/media/rmaFolder/Adventure%20Quiz%20Capcom%20World%202%20-Japan%20920611-.mra', 'rbf_file': '/media/rbfFolder/jtcps1_20220819.rbf'}]
 
 
-payload = {'value':'0bc860fd-5d17-4b7a-aa6f-d6315975aab0'}
+
 
 
 
@@ -218,7 +222,7 @@ payload = {'value':'0bc860fd-5d17-4b7a-aa6f-d6315975aab0'}
 #----------------------------------main code---------------------
 try:
     python_sucks = 0
-    r = requests.get('http://127.0.0.1:8000/account/userGame', params=payload) #getting account info
+    r = requests.get('http://'+ ip_addr +':8000/account/userGame', params=payload) #getting account info
     #r = requests.get('http://127.0.0.1:8000/media/rmaFolder/19XX_The_War_Against_Destiny_Euro_960104.mra', params=payload) #downloading the file
     #print(r.status_code )
     if r.status_code == 200:
@@ -238,119 +242,172 @@ try:
         #Create a new file
         f = open("serverList.txt","w")
         #print(server_list)
-#-------output the web server list to a file and add it to the hash table h_web. 
-        #I get division by zero in modulus operator
-        for x in range(web_num_games):
-            output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
-            f.write(output)
-            h_web.add(server_list[x]['game_name'], output)
-        f.close()
-        #h_web.print()
-        #print("-----------Above-server-Games--------------------\n")
 
-#-------Check if file exists and Check if the file is empty then Lines_test=0 else Lines_test>=1
-        if os.path.exists("misterList.txt"):
-            f_check = open("misterList.txt","r")
-            Lines_test = f_check.readlines()
-            f_check.close()
 
-#-------check if the misterList game file exists. if it does then add/delete the games
-        if os.path.exists("misterList.txt") and (len(Lines_test)>0):
-            #print("file exists!!\n")
-            #read in the file
-            f2 = open("misterList.txt","r")
-            #read the previous misterList.txt games
-            Lines = f2.readlines()
-            #get the size of the hash table
-            hash_size = math.ceil(1.3*len(Lines))
-            #create the Mister hashmap
-            h_mister = Hashmap(hash_size)
-            #The number of games currently in the misterList.txt
-            mister_num_games = len(Lines)
-            f2.close()
+        if server_list == []: #this when the server sends an empty list the hash wont be divided by zero
+            print("no list on server")
+    #-------Check if file exists and Check if the file is empty then Lines_test=0 else Lines_test>=1
+            if os.path.exists("misterList.txt"):
+                f_check = open("misterList.txt","r")
+                Lines_test = f_check.readlines()
+                f_check.close()
 
-            #print(len(Lines))
-#-----------Add in the games to the mister hash map
-            for x in range(mister_num_games): 
-                #get the current game string
-                game=Lines[x].split(',')
-                h_mister.add(game[0], Lines[x])
-            
-#-----------#check to see if the misterList.txt games are on the web hash map, If not remove them from the Hash and delete the file
-            for x in range(mister_num_games): 
-                game=Lines[x].split(',')
-                #print(game[0])
-                temp = h_web.get(game[0])
-                #print(temp)
-                if temp == "1":
-                    python_sucks =0
-                    #print("Do nothing")
-                    #game[0]=game_name,   Lines=game download link and path
-                    #h_mister.add(game[0], Lines[x])
-                else:
+    #-------check if the misterList game file exists. if it does then delete all the game on the mister list
+            if os.path.exists("misterList.txt") and (len(Lines_test)>0):
+                print("there are games on mister")
+                #print("file exists!!\n")
+                #read in the file
+                f2 = open("misterList.txt","r")
+                #read the previous misterList.txt games
+                Lines = f2.readlines()
+                #get the size of the hash table
+                hash_size = math.ceil(1.3*len(Lines))
+                #create the Mister hashmap
+                h_mister = Hashmap(hash_size)
+                #The number of games currently in the misterList.txt
+                mister_num_games = len(Lines)
+                f2.close()
+                #print(len(Lines))
+    #-----------Add in the games to the mister hash map
+                for x in range(mister_num_games): 
+                    #get the current game string
+                    game=Lines[x].split(',')
+                    h_mister.add(game[0], Lines[x])
+                
+    #-----------#check to see if the misterList.txt games are on the web hash map, If not remove them from the Hash and delete the file
+                for x in range(mister_num_games): 
                     #print("Remove the game here the function to remove the game file")
                     #print("Delete")
+                    game=Lines[x].split(',')
+                    print("Delete")
+                    print(game[0])
                     h_mister.delete(game[0])
                     delete_Game(Lines[x])
+                    f3 = open("misterList.txt","w") #write an empty file
 
-                #print("\n")
-            #print("-----------Above-delete-Games--------------------\n\n")
-
-
-#-----------#check to see if the web games are on the mister hash map, If not add them to the hash map and add the files
-            
-            #read in the web server list
-            f3 = open("serverList.txt","r")
-            #read the previous misterList.txt games
-            Lines = f3.readlines()
-            #h_mister.print()  
-            #print("((((((((((()))))))))))")
-            temp = h_mister.get('Billiard')
-            #print(temp)
-            f3.close()
-            for x in range(web_num_games): 
-                game=Lines[x].split(',')
-                temp = h_mister.get((game[0]))
-                #print((game[0].strip('\n')))
-                #print(temp)
-                if temp == "1":
-                    python_sucks = 0
-                    #print("Do nothing")
-                    #game[0]=game_name,   Lines=game download link and path
-                    #h_mister.add(game[0], Lines[x])
-                else:
-                    #print("Remove the game here the function to remove the game file")
-                    #print("ADD")
-                    h_mister.add(game[0],Lines[x])
-                    #print(Lines[x])
-                    write_Game(Lines[x])
                 
-                #print("\n")
+            else:
+                #do nothing the user is syncing with no games on the list and the mister file is empty
+                python_sucks = 1
+                print("no games on mister and server list is empty. do nothing")
 
-            #Output the hashmap to the misterfile
 
-            #h_mister.print()  
-            h_mister.output_file()
-            #print(h_mister.print())
-            #print(h_mister.get("game2"))
 
-            #print("-----------Above-ADD-Games--------------------\n")
 
-#-------if the file does not axist then create it
-        else:
-            f3 = open("misterList.txt","w")
+
+
+#-------This means that the serve list is not empty so add/delete the games
+        else:   
+    #-------output the web server list to a file and add it to the hash table h_web. 
+            #I get division by zero in modulus operator
             for x in range(web_num_games):
-                #print("file does not exists!!")
                 output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
-                f3.write(output)
-                h_web.add(server_list[x]['game_name'], output) #add to hashmap
-                write_Game(output)
+                f.write(output)
+                h_web.add(server_list[x]['game_name'], output)
+            f.close()
+            #h_web.print()
+            #print("-----------Above-server-Games--------------------\n")
+
+    #-------Check if file exists and Check if the file is empty then Lines_test=0 else Lines_test>=1
+            if os.path.exists("misterList.txt"):
+                f_check = open("misterList.txt","r")
+                Lines_test = f_check.readlines()
+                f_check.close()
+
+    #-------check if the misterList game file exists. if it does then add/delete the games
+            if os.path.exists("misterList.txt") and (len(Lines_test)>0):
+                #print("file exists!!\n")
+                #read in the file
+                f2 = open("misterList.txt","r")
+                #read the previous misterList.txt games
+                Lines = f2.readlines()
+                #get the size of the hash table
+                hash_size = math.ceil(1.3*len(Lines))
+                #create the Mister hashmap
+                h_mister = Hashmap(hash_size)
+                #The number of games currently in the misterList.txt
+                mister_num_games = len(Lines)
+                f2.close()
+
+                #print(len(Lines))
+    #-----------Add in the games to the mister hash map
+                for x in range(mister_num_games): 
+                    #get the current game string
+                    game=Lines[x].split(',')
+                    h_mister.add(game[0], Lines[x])
+                
+    #-----------#check to see if the misterList.txt games are on the web hash map, If not remove them from the Hash and delete the file
+                for x in range(mister_num_games): 
+                    game=Lines[x].split(',')
+                    #print(game[0])
+                    temp = h_web.get(game[0])
+                    #print(temp)
+                    if temp == "1":
+                        python_sucks =0
+                        #print("Do nothing")
+                        #game[0]=game_name,   Lines=game download link and path
+                        #h_mister.add(game[0], Lines[x])
+                    else:
+                        #print("Remove the game here the function to remove the game file")
+                        #print("Delete")
+                        h_mister.delete(game[0])
+                        delete_Game(Lines[x])
+
+                    #print("\n")
+                #print("-----------Above-delete-Games--------------------\n\n")
 
 
+    #-----------#check to see if the web games are on the mister hash map, If not add them to the hash map and add the files
+                
+                #read in the web server list
+                f3 = open("serverList.txt","r")
+                #read the previous misterList.txt games
+                Lines = f3.readlines()
+                #h_mister.print()  
+                #print("((((((((((()))))))))))")
+                temp = h_mister.get('Billiard')
+                #print(temp)
+                f3.close()
+                for x in range(web_num_games): 
+                    game=Lines[x].split(',')
+                    temp = h_mister.get((game[0]))
+                    #print((game[0].strip('\n')))
+                    #print(temp)
+                    if temp == "1":
+                        python_sucks = 0
+                        #print("Do nothing")
+                        #game[0]=game_name,   Lines=game download link and path
+                        #h_mister.add(game[0], Lines[x])
+                    else:
+                        #print("Remove the game here the function to remove the game file")
+                        #print("ADD")
+                        h_mister.add(game[0],Lines[x])
+                        #print(Lines[x])
+                        write_Game(Lines[x])
+                    
+                    #print("\n")
+
+                #Output the hashmap to the misterfile
+
+                #h_mister.print()  
+                h_mister.output_file()
+                #print(h_mister.print())
+                #print(h_mister.get("game2"))
+
+                #print("-----------Above-ADD-Games--------------------\n")
+
+    #-------if the file does not axist then create it
+            else:
+                f3 = open("misterList.txt","w")
+                for x in range(web_num_games):
+                    #print("file does not exists!!")
+                    output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
+                    f3.write(output)
+                    h_web.add(server_list[x]['game_name'], output) #add to hashmap
+                    write_Game(output)
 
 
-
-            f3.close()
+                f3.close()
 
 
 
