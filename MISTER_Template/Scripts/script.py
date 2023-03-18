54aebeaa40a9270f084653d250fc6a150225fc32
JoseC678 <cervantesjose657@gmail.com>
2023-03-17 18:27:54 -0700

    added core add/delete

--- MISTER_Template/Scripts/script.py
+++ MISTER_Template/Scripts/script.py
@@ -90,133 +90,154 @@ class Hashmap:
         f5.close()
     
 def write_Game(text):
-    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', '/media/rbfFolder/jtcps1_20220819.rbf','https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
+    print(text)
+    
+    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', '/media/rbfFolder/jtcps1_20220819.rbf','arcade','https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
     game = text.split(',')
+    if game[3] == "arcade":
+        print("arcade")
+        #get the rma file replace the %20 characters that show up from .get()
+        #print((game[1].replace('%20',' ')))
+        split = (game[1].replace('%20',' ')).split('/')
+        mra = split[3]
 
-    #get the rma file replace the %20 characters that show up from .get()
-    #print((game[1].replace('%20',' ')))
-    split = (game[1].replace('%20',' ')).split('/')
-    mra = split[3]
-
-    split = (game[2].replace('%20',' ')).split('/')
-    rbf = split[3]
-
-
-
-    #write the mra file
-    r4 = requests.get('http://' + ip_addr + ':8000/media/rmaFolder/' + mra)
-    with open('../_Arcade/'+mra,'wb') as fx:
-        fx.write(r4.content)
-    python_sucks = 0
-
-    #write the rbf file
-    if os.path.exists('../_Arcade/cores/'+rbf):
-        python_sucks = python_sucks + 1
-    else:
-        print("Writing rbf file")
-        r5 = requests.get('http://'+ ip_addr + ':8000/media/rbfFolder/' + rbf)
-        with open('../_Arcade/cores/'+rbf,'wb') as fy:
-            fy.write(r5.content)
-
-    
-
-    #check how many files need to be downloaded subtract off the mra file and game name
-    #divide for how many pairs of files and paths
-    num_files = int((len(game) - 2)/2)
-    file_link = [] #['https://archive.org/download/mame.0229/bilyard.zip']    
-    file_path = [] #['games/mame/athena.zip\n']
+        split = (game[2].replace('%20',' ')).split('/')
+        rbf = split[3]
 
 
-    for i in range(num_files):
-        file_link.append(game[i+3]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
-    
-    for i in range(num_files):
-        file_path.append(game[(num_files+i+3)]) #plus 2 start at the first download link skip 'game_name' and 'rma' and rbf
-                                          #plus num_files to skip over the file_link
 
-    
-    # print(num_files)
-    # print("file_link") #['https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip']
-    # print(str(file_link))
-    # print("\n")
-    # print("file_path") #['games/mame/azurian.zip\n']
-    # print(str(file_path))
-    # print("\n")
+        #write the mra file
+        r4 = requests.get('http://' + ip_addr + ':8000/media/rmaFolder/' + mra)
+        with open('../_Arcade/'+mra,'wb') as fx:
+            fx.write(r4.content)
+        python_sucks = 0
 
-    #output the files. the Zip files
-    for i in range(num_files):
-        r1 = requests.get((file_link[i].strip('\n')).strip(' '))
-        #get file
-        with open('../'+(file_path[i].strip('\n')).strip(' '),'wb') as fx:
-            fx.write(r1.content)
+        #write the rbf file
+        if os.path.exists('../_Arcade/cores/'+rbf):
+            python_sucks = python_sucks + 1
+        else:
+            print("Writing rbf file")
+            r5 = requests.get('http://'+ ip_addr + ':8000/media/rbfFolder/' + rbf)
+            with open('../_Arcade/cores/'+rbf,'wb') as fy:
+                fy.write(r5.content)
 
+        
+
+        #check how many files need to be downloaded subtract off the mra file and game name
+        #divide for how many pairs of files and paths
+        num_files = int((len(game) - 3)/2)
+        file_link = [] #['https://archive.org/download/mame.0229/bilyard.zip']    
+        file_path = [] #['games/mame/athena.zip\n']
+
+
+        for i in range(num_files):
+            file_link.append(game[i+4]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf and game_type
+        
+        for i in range(num_files):
+            file_path.append(game[(num_files+i+4)]) #plus 2 start at the first download link skip 'game_name' and 'rma' and rbf and game_type
+                                            #plus num_files to skip over the file_link
+
+        
+        # print(num_files)
+        # print("file_link") #['https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip']
+        # print(str(file_link))
+        # print("\n")
+        # print("file_path") #['games/mame/azurian.zip\n']
+        # print(str(file_path))
+        # print("\n")
+
+        #output the files. the Zip files
+        for i in range(num_files):
+            r1 = requests.get((file_link[i].strip('\n')).strip(' '))
+            #get file
+            with open('../'+(file_path[i].strip('\n')).strip(' '),'wb') as fx:
+                fx.write(r1.content)
+    elif game[3]== "core":
+        split = (game[1].replace('%20',' ')).split('/')
+        game_folder = split[3]
+        print(game_folder) #GBA
+
+        split = (game[2].replace('%20',' ')).split('/')
+        zip_name = split[3] #megaman.zip
+        print(zip_name)
+
+        #write the mra file
+        r4 = requests.get('http://' + ip_addr + ':8000/media/rmaFolder/' + game_folder)
+        with open('../games/'+game_folder+'/'+zip_name,'wb') as fx:
+            fx.write(r4.content)
+        
 
 #same as write_Game but now delete
 def delete_Game(text):
-    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', 'https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
+    
+    #split up the file ['Azurian Attack', '/media/rmaFolder/Azurian%20Attack.mra', 'arcade' ,'https://archive.org/download/MAME220RomsOnlyMerged/azurian.zip', 'games/mame/azurian.zip\n']
     game = text.split(',')
+    if game[3] == "arcade":
+        #get the rma file replace the %20 characters that show up from .get()
+        #print((game[1].replace('%20',' ')))
+        split = (game[1].replace('%20',' ')).split('/')
+        mra = split[3]
 
-    #get the rma file replace the %20 characters that show up from .get()
-    #print((game[1].replace('%20',' ')))
-    split = (game[1].replace('%20',' ')).split('/')
-    mra = split[3]
-
-    split = (game[2].replace('%20',' ')).split('/')
-    rbf = split[3]
-
-
-    
-    #delete MRA file
-    if os.path.exists('../_Arcade/'+mra):
-        os.remove('../_Arcade/'+mra)
-    # else:
-        #do not remove the rbf file since one core supports multiple arcade games
-        #print("File does not exist")
+        split = (game[2].replace('%20',' ')).split('/')
+        rbf = split[3]
 
 
-    #check how many files need to be downloaded subtract off the mra file and game name
-    #divide for how many pairs of files and paths
-    num_files = int((len(game) - 2)/2)
-
-    file_link = [] #['https://archive.org/download/mame.0229/bilyard.zip']    
-    file_path = [] #['games/mame/athena.zip\n']
-
-    for i in range(num_files):
-        file_link.append(game[i+3]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
-    
-    for i in range(num_files):
-        file_path.append(game[(num_files+i+3)]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf
-                                          #plus num_files to skip over the file_link
-
-    # zip_file = file_link[0].split('/') #['https:', '', 'archive.org', 'download', 'MAME216RomsOnlyMerged', '1942.zip']
-    # last_elem = (len(zip_file) - 1) #last element in the array is the file name
-    # print("zip_file")
-    # print(last_elem)
-
-
-    #delete the zip files
-    for i in range(num_files):
-        if os.path.exists('../'+(file_path[i].strip('\n')).strip(' ')):
-            os.remove('../'+(file_path[i].strip('\n')).strip(' '))
+        
+        #delete MRA file
+        if os.path.exists('../_Arcade/'+mra):
+            os.remove('../_Arcade/'+mra)
         # else:
-        #     print("File does not exist")
+            #do not remove the rbf file since one core supports multiple arcade games
+            #print("File does not exist")
 
-    #delete MRA file
 
+        #check how many files need to be downloaded subtract off the mra file and game name
+        #divide for how many pairs of files and paths
+        num_files = int((len(game) - 3)/2)
+
+        file_link = [] #['https://archive.org/download/mame.0229/bilyard.zip']    
+        file_path = [] #['games/mame/athena.zip\n']
+
+        for i in range(num_files):
+            file_link.append(game[i+4]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf and game_type
+        
+        for i in range(num_files):
+            file_path.append(game[(num_files+i+4)]) #plus 3 start at the first download link skip 'game_name' and 'rma' and rbf and game_type
+                                            #plus num_files to skip over the file_link
+
+        # zip_file = file_link[0].split('/') #['https:', '', 'archive.org', 'download', 'MAME216RomsOnlyMerged', '1942.zip']
+        # last_elem = (len(zip_file) - 1) #last element in the array is the file name
+        # print("zip_file")
+        # print(last_elem)
+
+
+        #delete the zip files
+        for i in range(num_files):
+            if os.path.exists('../'+(file_path[i].strip('\n')).strip(' ')):
+                os.remove('../'+(file_path[i].strip('\n')).strip(' '))
+            # else:
+            #     print("File does not exist")
+
+        #delete MRA file
+    elif game[3]== "core":
+        split = (game[1].replace('%20',' ')).split('/')
+        game_folder = split[3]
+        print(game_folder) #GBA
+
+        split = (game[2].replace('%20',' ')).split('/')
+        zip_name = split[3] #megaman.zip
+        print(zip_name)
+        
+        #delete MRA file
+        if os.path.exists('../games/'+game_folder+'/'+zip_name):
+            os.remove('../games/'+game_folder+'/'+zip_name)
+        # else:
+            #do not remove the rbf file since one core supports multiple arcade games
+            #print("File does not exist")
 
 #SENDING 2 GAMES [{} ,{}]
 # [{'id': 1, 'game_name': '4D Warriors (315-5162)', 'game_file_link': 'https://archive.org/download/MAME216RomsOnlyMerged/4dwarrio.zip', 'file_location': 'games/mame/4dwarrio.zip', 'game_year': 9999, 'cover': 'https://www.igdb.com/ find the image', 'file_size': 10.0, 'rma_file': '/media/rmaFolder/4D%20Warriors%20(315-5162).mra', 'rbf_file': '/media/rbfFolder/SEGASYS1_20210618.rbf'},
 # {'id': 4, 'game_name': 'Adventure Quiz Capcom World 2 -Japan 920611-', 'game_file_link': 'https://archive.org/download/MAME217RomsOnlyMerged/MAME%200.217%20ROMs%20%28merged%29.zip/cworld2j.zip', 'file_location': 'games/mame/cworld2j.zip', 'game_year': 1941, 'cover': 'https://www.igdb.com/ find the image', 'file_size': 10.0, 'rma_file': '/media/rmaFolder/Adventure%20Quiz%20Capcom%20World%202%20-Japan%20920611-.mra', 'rbf_file': '/media/rbfFolder/jtcps1_20220819.rbf'}]
-
-
-
-
-
-
-
-
-
-
 
 
 #----------------------------------main code---------------------
@@ -301,7 +322,7 @@ try:
     #-------output the web server list to a file and add it to the hash table h_web. 
             #I get division by zero in modulus operator
             for x in range(web_num_games):
-                output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
+                output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_type']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
                 f.write(output)
                 h_web.add(server_list[x]['game_name'], output)
             f.close()
@@ -401,7 +422,7 @@ try:
                 f3 = open("misterList.txt","w")
                 for x in range(web_num_games):
                     #print("file does not exists!!")
-                    output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
+                    output = (server_list[x]['game_name']+","+server_list[x]['rma_file']+","+server_list[x]['rbf_file']+","+server_list[x]['game_type']+","+server_list[x]['game_file_link']+","+server_list[x]['file_location']+"\n")
                     f3.write(output)
                     h_web.add(server_list[x]['game_name'], output) #add to hashmap
                     write_Game(output)
