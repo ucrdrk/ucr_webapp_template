1d26d00c2b9355889c680ad42422323b530b63c0
JoseC678 <cervantesjose657@gmail.com>
2023-03-19 05:50:17 -0700

    Added PUT request and deleted the misterList.txt file

--- MISTER_Template/Scripts/Cloud2FPGA/sync_script.py
+++ MISTER_Template/Scripts/Cloud2FPGA/sync_script.py
@@ -19,15 +19,23 @@ ip_addr = get_data("ip_addr.txt")
 global payload 
 user_id = get_data("id_user.txt")
 payload = {'value':user_id}
+payload2 = {'value':'f642b035-f46e-4d34-ae43-6e600fe405dd', 'play_flag':'dsfsd'}
 
 
 try:
     r = requests.get('http://'+ ip_addr +':8000/account/UserSync', params=payload) #getting account info
-
+    print("get")
+    print(r.status_code)
     if r.status_code == 200:
         server_variables = json.loads(r.text)
         print(server_variables)
 
+    print("put")
+    r1 = requests.put('http://'+ ip_addr +':8000/account/UserSync', data=json.dumps(payload2))
+    print(r1.status_code)
+    print(r1.content)
+    # print((payload2))
+    # print(json.dumps(payload2))
 
 
 except requests.exceptions.RequestException as e:
