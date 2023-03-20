bf43967b526268282998d2b2f5a0a7409bd05cde
JoseC678 <cervantesjose657@gmail.com>
2023-03-20 11:57:20 -0700

    Added path for post

--- account/api/urls.py
+++ account/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls import url
 from account.api import views
 from account.api.views import(
-    registration_view,userGameAPI,UserSyncAPI
+    registration_view,userGameAPI,UserSyncAPI,ChildCreateView,UpdateUserGameAPI
 )
 
 from rest_framework.authtoken.views import obtain_auth_token
@@ -11,11 +11,14 @@ app_name = "account"
 
 urlpatterns = [
     path('register', registration_view, name = 'register'),
+    path('parent/<int:pk>/child/create/', ChildCreateView.as_view(), name='create_child'),
     path('login', obtain_auth_token, name = 'login'),
     url(r'^userGame$',views.userGameAPI),
     url(r'^userGame/([0-9]+)$',views.userGameAPI),
     url(r'^UserSync$',views.UserSyncAPI),
-    url(r'^UserSync/([0-9]+)$',views.UserSyncAPI)
+    url(r'^UserSync/([0-9]+)$',views.UserSyncAPI),
+    url(r'^UpdateUserGame$',views.UpdateUserGameAPI),
+    url(r'^UpdateUserGame/([0-9]+)$',views.UpdateUserGameAPI)
 
     #url(r'^userGame/$', userGameAPI, name='userGame')
 ]
